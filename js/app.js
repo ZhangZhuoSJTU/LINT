8b8c08f29d3f7133021a2ec140209c138995493a
SolidShen <solidshen519@gmail.com>
2024-05-13 16:31:29 -0400

    init

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// JavaScript to handle mouseover and mouseout events
+var activeMethodPill = null;
+var activeScenePill = null;
+var activeModePill = null;
+var activeVidID = 0;
+var select = false;
+
+
+$(document).ready(function () {
+    var editor = CodeMirror.fromTextArea(document.getElementById("bibtex"), {
+        lineNumbers: false,
+        lineWrapping: true,
+        readOnly: true
+    });
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+
+    activeMethodPill = $('.method-pill').filter('.active')[0];
+    activeModePill = $('.mode-pill').filter('.active')[0];
+    activeScenePill = $('.scene-pill').filter('.active')[0];
+});
+
+function selectGif(methodPill, scenePill, n_views, modePill) {
+    select = true;
+
+    if (activeMethodPill) {
+        activeMethodPill.classList.remove("active");
+    }
+    if (activeScenePill) {
+        activeScenePill.classList.remove("active");
+    }
+    if (modePill) {
+        activeModePill.classList.remove("active");
+        modePill.classList.add("active");
+        activeModePill = modePill;
+    }
+    activeMethodPill = methodPill;
+    activeScenePill = scenePill;
+    methodPill.classList.add("active");
+    scenePill.classList.add("active");
+}
